48a7e10f92f026cea4011c2522cf44241625510a
SaniaMeer <saniameer2007@gmail.com>
2026-09-10 00:08:09 +0500

    completed features of expense tracker

--- Project 6 Expense Tracker/script.js
+++ Project 6 Expense Tracker/script.js
@@ -4,7 +4,7 @@ let amt = document.getElementById("amount")
 let type = document.getElementById("type")
 
 let balance = document.getElementById("balance")
-let income = document.getElementById("income")
+let incomedata = document.getElementById("income")
 let expenses = document.getElementById("expenses")
 
 function createTransaction() {
@@ -12,17 +12,44 @@ function createTransaction() {
     let list = document.createElement("li")
     let transaction = {
         description: descrp.value,
-        amount:amt.value,
-        type:type.value
+        amount: amt.value,
+        type: type.value
     }
     transactions.push(transaction)
-    list.textContent = `${transaction.description} - ${transaction.amount} - ${transaction.type}`; 
+    list.textContent = `${transaction.description} - ${transaction.amount} - ${transaction.type}`;
     ul.appendChild(list)
+    let incometotal;
+    let expensetotal;
+
+    let onlyincome = transactions.filter((currentValue) => {
+        return currentValue.type === "income";
+    });
+    incometotal = onlyincome.reduce((accumulator, currentValue) => {
+        return accumulator + Number(currentValue.amount);
+    }, 0);
+
+    if (transaction.type === "income") {
+
+        incomedata.textContent = "$" + incometotal
+    }
+
+    let ex = transactions.filter((currentValue) => {
+        return currentValue.type === "expense";
+    });
+    expensetotal = ex.reduce((accumulator, currentValue) => {
+        return accumulator + Number(currentValue.amount);
+    }, 0);
+
+    if (transaction.type === "expense") {
+
+        expenses.textContent = "$" + expensetotal
+    }
+    balance.textContent = "$" + (incometotal - expensetotal);
 }
 
 let button = document.getElementById('btn')
 
-button.addEventListener('click',(e)=>{
+button.addEventListener('click', (e) => {
     e.preventDefault();
-   createTransaction();
+    createTransaction();
 })
